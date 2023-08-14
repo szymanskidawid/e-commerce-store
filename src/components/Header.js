bccc7d1ba9687eb0a6d915fc02186dbea4b24c80
Dawid <szynanski@hotmail.com>
2023-08-14 22:26:18 +0200

    added comments, new button and prop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import Logo from "./Logo";
 import NavButton from "./NavButton";
 import CheckoutButton from "./CheckoutButton";
+import LightDarkButton from "./LightDarkButton";
 
-const Header = () => {
+// Header component containing logo and buttons.
+
+const Header = ({toggleCheckout}) => {
   return (
     <div>
       <header className='header-container'>
         <Logo />
         <nav className='nav-container'>
           <NavButton text='Products' />
-          <CheckoutButton />
+          <CheckoutButton onClick={toggleCheckout}/>
+          <LightDarkButton />
         </nav>
       </header>
     </div>
