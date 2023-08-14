e49d32eab320b41d88f8c970d116cd668fca9511
Dawid <szynanski@hotmail.com>
2023-08-14 22:26:34 +0200

    added code to show Basket

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React, { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ProductGrid from "./components/ProductGrid";
 import BasketGrid from "./components/BasketGrid";
 
 function App() {
+  //Below displays the basket.
+  const [checkoutMode, setCheckoutMode] = useState(false)
+
+  const toggleCheckout = () => {
+    console.log("Checkout button clicked");
+    setCheckoutMode(!checkoutMode);
+  }
+
   return (
     <div className="App">
-      <Header />
-      <ProductGrid />
-      <BasketGrid />
+      <Header toggleCheckout={toggleCheckout} />
+      {checkoutMode ? <BasketGrid /> : <ProductGrid />}
       <Footer />
     </div>
   );
