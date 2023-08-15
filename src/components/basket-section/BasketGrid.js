afe619167f01b67e9da85dc855300577eddfd031
Dawid <szynanski@hotmail.com>
2023-08-15 12:54:52 +0200

    work on basket section

---
+++ src/components/basket-section/BasketGrid.js
@@ -0,0 +1,18 @@
+// Container displaying items added to the basket.
+
+const BasketGrid = () => {
+  return (
+    <section >
+      <div className='scrolling-window'>
+        <div className='basket-scrolling-container'>
+          Basket
+        </div>
+        <div className="basket-info-container">
+
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default BasketGrid;
---
+++ src/components/basket-section/BasketProduct.js
@@ -0,0 +1,18 @@
+const BasketProduct = () => {
+  return (
+    <div className='basket-product-container'>
+      <div className='basket-product-picture'>
+
+      </div>
+      <div className='basket-product-info'>
+        
+      </div>
+      <div className='basket-product-remove-button'>
+        
+      </div>
+
+    </div>
+  );
+}
+
+export default BasketProduct;
