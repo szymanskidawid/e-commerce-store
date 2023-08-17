21b9e1712e2a12615a248e53614fcf71260a2d7b
Dawid <szynanski@hotmail.com>
2023-08-17 21:37:32 +0200

    new button with a remove icon

---
+++ src/components/buttons/RemoveButton.js
@@ -0,0 +1,9 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const RemoveButton = () => {
+  return (
+    <DeleteIcon className='basket-section-remove-button' /> 
+  );
+}
+
+export default RemoveButton;
