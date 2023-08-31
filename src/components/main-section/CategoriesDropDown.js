bc59408a497252bc660d5ce296f58846a4e76521
Dawid <szynanski@hotmail.com>
2023-08-31 21:17:06 +0200

    new component used to select categories

---
+++ src/components/main-section/CategoriesDropDown.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+
+
+const CategoriesDropDown = () => {
+  //Displays categories when hovered over the main label.
+  const [ isDropDownVisible, setIsDropDownVisible] = useState(false)
+
+  const handleHover = (isHovered) => {
+    setIsDropDownVisible(isHovered)
+  }
+
+  return (
+    <div className="categories-container">
+      <div className="categories-hover-container"
+      onMouseEnter={() => handleHover(true)}
+      onMouseLeave={() => handleHover(false)}>
+        Categories
+      </div>
+      <div className={`categories-dropdown-container 
+      ${isDropDownVisible ? 'visible' : ''}`}
+      onMouseEnter={() => handleHover(true)}
+      onMouseLeave={() => handleHover(false)}>
+        <FormGroup>
+          <FormControlLabel control={<Checkbox defaultChecked />} label="Food" />
+          <FormControlLabel control={<Checkbox defaultChecked />} label="Kitchen" />
+          <FormControlLabel control={<Checkbox defaultChecked />} label="Electronics" />
+        </FormGroup>
+      </div>
+    </div> 
+  );
+}
+
+export default CategoriesDropDown;
