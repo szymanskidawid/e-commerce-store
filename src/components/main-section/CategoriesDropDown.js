c72360af4bffac6078b9358dd5b49cf7274fbf10
Dawid <szynanski@hotmail.com>
2023-09-01 22:06:09 +0200

    passed props from ProductGrid to change states

--- src/components/main-section/CategoriesDropDown.js
+++ src/components/main-section/CategoriesDropDown.js
@@ -2,38 +2,46 @@ import React, { useState } from 'react';
 import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 
 
-const CategoriesDropDown = () => {
+const CategoriesDropDown = ({ isFoodSelected, isKitchenSelected, isElectronicsSelected,setIsFoodSelected, setIsKitchenSelected, setIsElectronicsSelected }) => {
   //Displays categories when hovered over the main label.
-  const [ isDropDownVisible, setIsDropDownVisible] = useState(false)
+  const [isDropDownVisible, setIsDropDownVisible] = useState(false);
 
-  const handleHover = (isHovered) => {
-    setIsDropDownVisible(isHovered)
+  const handleHover = isHovered => {
+    setIsDropDownVisible(isHovered);
   }
 
   return (
-    <div className="categories-container">
-      <div className="categories-hover-container"
+    <div
+      className="categories-container"
       onMouseEnter={() => handleHover(true)}
-      onMouseLeave={() => handleHover(false)}>
+      onMouseLeave={() => handleHover(false)}
+    >
+      <div className="categories-hover-container">
         Categories
       </div>
-      <div className={`categories-dropdown-container 
-      ${isDropDownVisible ? 'visible' : ''}`}
-      onMouseEnter={() => handleHover(true)}
-      onMouseLeave={() => handleHover(false)}>
+      <div
+        className={`categories-dropdown-container 
+          ${isDropDownVisible ? 'visible' : ''}`}
+      >
         <FormGroup>
           <FormControlLabel control={
-            <Checkbox style={{ color: 'green' }} defaultChecked />} 
-            label="Food" />
+            <Checkbox style={{ color: 'green' }} defaultChecked />}
+            label="Food" 
+            onClick={() => setIsFoodSelected(!isFoodSelected)}
+          />
           <FormControlLabel control={
-            <Checkbox style={{ color: 'green' }} defaultChecked />} 
-            label="Kitchen" />
+            <Checkbox style={{ color: 'green' }} defaultChecked />}
+            label="Kitchen"
+            onClick={() => setIsKitchenSelected(!isKitchenSelected)}
+          />
           <FormControlLabel control={
-            <Checkbox style={{ color: 'green' }} defaultChecked />} 
-            label="Electronics" />
+            <Checkbox style={{ color: 'green' }} defaultChecked />}
+            label="Electronics"
+            onClick={() => setIsElectronicsSelected(!isElectronicsSelected)} 
+          />
         </FormGroup>
       </div>
-    </div> 
+    </div>
   );
 }
 
