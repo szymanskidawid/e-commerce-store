4a818e7eed8aed2b088275a7afbf20657333c567
Dawid <szynanski@hotmail.com>
2023-09-01 22:04:37 +0200

    improved look of return state

--- src/components/buttons/RemoveButton.js
+++ src/components/buttons/RemoveButton.js
@@ -2,10 +2,8 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 // Button used inside BasketProduct to remove a product from the basket.
 
-const RemoveButton = () => {
-  return (
-    <DeleteIcon className='basket-section-remove-button' /> 
-  );
-}
+const RemoveButton = () => (
+  <DeleteIcon className='basket-section-remove-button' /> 
+)
 
 export default RemoveButton;
--- src/components/header-section/Logo.js
+++ src/components/header-section/Logo.js
@@ -1,11 +1,9 @@
 // Website logo visible in the Header component.
 
-const Logo = () => {
-  return (
-    <div className='website-logo'>
-      <img src="https://picsum.photos/150/60" />
-    </div>
-  );
-}
+const Logo = () => (
+  <div className='website-logo'>
+    <img src="https://picsum.photos/150/60" />
+  </div>
+)
 
 export default Logo;
