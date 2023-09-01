b6eb1600751eac058217cf38b19962c7e815010f
Dawid <szynanski@hotmail.com>
2023-09-01 22:07:08 +0200

    added fetch from JSON server, states of categories

--- src/components/main-section/ProductGrid.js
+++ src/components/main-section/ProductGrid.js
@@ -1,33 +1,60 @@
+import { useEffect, useState } from "react";
 import CategoriesDropDown from "./CategoriesDropDown";
 import ProductCard from "./ProductCard";
 
 // Container displaying items available to purchase.
 
 const ProductGrid = ({ darkMode }) => {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:4000/products');
+
+      const dataJSON = await response.json();
+
+      setData(dataJSON);
+    }
+
+    fetchData();
+  }, [])
+
+  const [isFoodSelected, setIsFoodSelected] = useState(true);
+  const [isKitchenSelected, setIsKitchenSelected] = useState(true);
+  const [isElectronicsSelected, setIsElectronicsSelected] = useState(true);
+
+  console.log("////");
+  console.log({isFoodSelected});
+  console.log({isKitchenSelected});
+  console.log({isElectronicsSelected});
+  console.log("////");
+
   return (
     <main>
-      <CategoriesDropDown />
+      <CategoriesDropDown
+        isFoodSelected={isFoodSelected}
+        isKitchenSelected={isKitchenSelected}
+        isElectronicsSelected={isElectronicsSelected}
+        setIsFoodSelected={setIsFoodSelected}
+        setIsKitchenSelected={setIsKitchenSelected}
+        setIsElectronicsSelected={setIsElectronicsSelected}
+      />
       <div className="product-grid-container">
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
-        <ProductCard darkMode={darkMode} />
+        {data === null ? <div>Loading...</div> :
+          data.map(product => {
+
+
+
+            return (
+              <ProductCard 
+                darkMode={darkMode}
+                name={product.name}
+                price={product.price} 
+              />
+            )
+          })
+        }
       </div>
     </main>
   );
