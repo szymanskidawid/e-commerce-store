378fb2e0a293df30793267e95aa8661ef8c07791
Dawid <szynanski@hotmail.com>
2023-09-03 13:38:30 +0200

    added state and visuals for empty basket

--- src/components/basket-section/BasketGrid.js
+++ src/components/basket-section/BasketGrid.js
@@ -1,21 +1,29 @@
+import { useState } from "react";
 import MainButton from "../buttons/MainButton";
 import BasketProduct from "./BasketProduct";
 
 const BasketGrid = ({ basketTotal, darkMode }) => {
+  const [isBasketEmpty, setIsBasketEmpty] = useState(true);
+
   return (
     <section >
       {/* Ternary operator to change between light and dark mode classes. */}
       <div className={`scrolling-window ${darkMode
       ? 'scrolling-window-dark-theme'
       : 'scrolling-window-light-theme'}`}>
-        <div className='basket-scrolling-container'>
-          <BasketProduct darkMode={darkMode} />
-          <BasketProduct darkMode={darkMode} />
-          <BasketProduct darkMode={darkMode} />
-          <BasketProduct darkMode={darkMode} />
-          <BasketProduct darkMode={darkMode} />
-          <BasketProduct darkMode={darkMode} />
-        </div>
+        {isBasketEmpty === true ? 
+          <div className="basket-empty">
+            Basket is Empty
+          </div> :
+          <div className='basket-scrolling-container'>
+            <BasketProduct darkMode={darkMode} />
+            <BasketProduct darkMode={darkMode} />
+            <BasketProduct darkMode={darkMode} />
+            <BasketProduct darkMode={darkMode} />
+            <BasketProduct darkMode={darkMode} />
+            <BasketProduct darkMode={darkMode} />
+          </div>
+        }
       </div>
       <div className="basket-bottom-container">
         <div className="basket-bottom-price">
