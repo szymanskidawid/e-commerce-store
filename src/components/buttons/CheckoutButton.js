a7b287bb2c0d0e897515d3e6ab7f8a143931c4e8
Dawid <szynanski@hotmail.com>
2023-09-08 20:44:17 +0200

    made icon disappear when basket is empty

--- src/components/buttons/CheckoutButton.js
+++ src/components/buttons/CheckoutButton.js
@@ -6,7 +6,9 @@ const CheckoutButton = ({ color, onClick, basketItems }) => {
   return (
     <Button className="checkout-button" variant="contained" color={color} onClick={onClick}>
       <ShoppingCartIcon style={{ fontSize: 45 }} />
-      <div className="checkout-button-products-amount-label">{basketItems}</div>
+      <div className="checkout-button-products-amount-label" style={{ display: basketItems === 0 ? 'none' : 'flex' }}>
+        {basketItems}
+      </div>
     </Button>
   );
 };
