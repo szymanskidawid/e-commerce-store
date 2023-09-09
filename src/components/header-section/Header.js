43f00724cc0c8efd3f3ade95e5b1f5f9c726c5a8
Dawid <szynanski@hotmail.com>
2023-09-09 19:24:29 +0200

    added display of basketTotal to NavBar

--- src/components/header-section/Header.js
+++ src/components/header-section/Header.js
@@ -3,13 +3,16 @@ import MainButton from '../buttons/MainButton';
 import CheckoutButton from '../buttons/CheckoutButton';
 import LightDarkButton from '../buttons/LightDarkButton';
 
-const Header = ({ toggleCheckout, colorMode, darkMode, basketItems }) => (
+const Header = ({ toggleCheckout, colorMode, darkMode, basketItems, basketTotal }) => (
   <div>
     <header className={darkMode ? 'header-dark-theme' : 'header-light-theme'}>
       <Logo />
       <nav className="nav-container">
         <MainButton className={'nav-section-products-button'} text={'Products'} />
-        <CheckoutButton onClick={toggleCheckout} basketItems={basketItems} />
+        <div className="nav-section-basket-components">
+          <p className="nav-section-basket-total">{basketTotal} zł</p>
+          <CheckoutButton onClick={toggleCheckout} basketItems={basketItems} />
+        </div>
         <LightDarkButton onClick={colorMode} />
       </nav>
     </header>
