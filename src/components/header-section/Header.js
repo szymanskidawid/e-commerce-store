6f0e747fe0cb689635830cfb290877de900b1aeb
Dawid <szynanski@hotmail.com>
2023-09-11 23:51:16 +0200

    added functionalities to buttons, RWD fixes

--- src/components/header-section/Header.js
+++ src/components/header-section/Header.js
@@ -2,21 +2,32 @@ import Logo from './Logo';
 import MainButton from '../buttons/MainButton';
 import CheckoutButton from '../buttons/CheckoutButton';
 import LightDarkButton from '../buttons/LightDarkButton';
+import { Link } from 'react-router-dom';
 
-const Header = ({ toggleCheckout, colorMode, darkMode, basketItems, basketTotal }) => (
-  <div>
-    <header className={darkMode ? 'header-dark-theme' : 'header-light-theme'}>
-      <Logo />
-      <nav className="nav-container">
-        <MainButton className={'nav-section-products-button'} text={'Products'} />
-        <div className="nav-section-basket-components">
-          <p className="nav-section-basket-total">{basketTotal} zł</p>
-          <CheckoutButton onClick={toggleCheckout} basketItems={basketItems} />
-        </div>
-        <LightDarkButton onClick={colorMode} />
-      </nav>
-    </header>
-  </div>
-);
+const Header = ({ colorMode, darkMode, basketItems, basketTotal }) => {
+  return (
+    <div>
+      <header className={darkMode ? 'header-dark-theme' : 'header-light-theme'}>
+        <Logo />
+        <nav className="nav-container">
+          <div className="nav-products-button-container">
+            <Link className="nav-section-link" to="/products">
+              <MainButton className={'nav-section-products-button'} text={'Products'} />
+            </Link>
+          </div>
+          <div className="nav-section-basket-components">
+            <p className="nav-section-basket-total">{basketTotal} zł</p>
+            <Link className="nav-section-link" to="/basket">
+              <CheckoutButton basketItems={basketItems} />
+            </Link>
+          </div>
+          <div className="nav-theme-button-container">
+            <LightDarkButton onClick={colorMode} />
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
 
 export default Header;
