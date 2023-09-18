17982e8bed7c30584f4d1be9946642b8c43a4dc8
Dawid <szynanski@hotmail.com>
2023-09-18 22:36:38 +0200

    improved function locations, fixed routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import Footer from './components/Footer';
-import Header from './components/header-section/Header';
 import ProductGrid from './components/main-section/ProductGrid';
 import BasketGrid from './components/basket-section/BasketGrid';
 import ContextProviders from './contexts/ContextProviders';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { fetchData } from './helpers/fetchData';
+import Page from './components/Page';
 
 function App() {
-  //Changes between Light and Dark mode.
   const [darkMode, setDarkMode] = useState(false);
+  const [data, setData] = useState(null);
+  const [basket, setBasket] = useState({});
 
   useEffect(() => {
     if (darkMode) {
@@ -23,68 +23,10 @@ function App() {
     };
   }, [darkMode]);
 
-  const [data, setData] = useState(null);
-
-  //Data fetch call
   useEffect(() => {
     fetchData(setData);
   }, []);
 
-  //Below displays the basket.
-  const [basket, setBasket] = useState({});
-
-  const [isBasketEmpty, setIsBasketEmpty] = useState(true);
-
-  //Basket functionalities
-  const addToBasket = (product) => {
-    const newBasket = { ...basket };
-
-    if (newBasket[product.id] >= 1) {
-      setBasket({ ...newBasket, [product.id]: newBasket[product.id] + 1 });
-    } else {
-      setBasket({ ...newBasket, [product.id]: 1 });
-    }
-
-    setIsBasketEmpty(false);
-  };
-
-  const decrementQuantity = (id) => {
-    const newBasket = { ...basket };
-    if (newBasket[id] > 1) {
-      setBasket({ ...newBasket, [id]: newBasket[id] - 1 });
-    }
-  };
-
-  const incrementQuantity = (id) => {
-    const newBasket = { ...basket };
-    if (newBasket[id] < data.find((item) => item.id === id).stock) {
-      setBasket({ ...newBasket, [id]: newBasket[id] + 1 });
-    }
-  };
-
-  const deleteProductFromBasket = (id) => {
-    const newBasket = { ...basket };
-    delete newBasket[id];
-    setBasket(newBasket);
-    if (Object.keys(newBasket).length === 0) {
-      setIsBasketEmpty(true);
-    }
-  };
-
-  const reduceStock = (product, quantity) => {
-    return fetch(`http://localhost:4000/products/${product.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        ...product,
-        stock: product.stock - quantity,
-      }),
-    });
-  };
-
-  //Other
   const totalPrice = () => {
     let price = 0;
     Object.keys(basket).forEach((id) => {
@@ -97,30 +39,18 @@ function App() {
     {
       path: '/products',
       element: (
-        <>
-          <Header basketItemsAmount={Object.keys(basket).length} basketTotalPrice={totalPrice()} />
-          <ProductGrid addToBasket={addToBasket} />
-          <Footer />
-        </>
+        <Page basket={basket} totalPrice={totalPrice}>
+          <ProductGrid />
+        </Page>
       ),
     },
 
     {
       path: '/basket',
       element: (
-        <>
-          <Header basketItemsAmount={Object.keys(basket).length} basketTotalPrice={totalPrice()} />
-          <BasketGrid
-            basketTotalPrice={totalPrice()}
-            decrementQuantity={decrementQuantity}
-            incrementQuantity={incrementQuantity}
-            isBasketEmpty={isBasketEmpty}
-            setIsBasketEmpty={setIsBasketEmpty}
-            deleteProductFromBasket={deleteProductFromBasket}
-            reduceStock={reduceStock}
-          />
-          <Footer />
-        </>
+        <Page basket={basket} totalPrice={totalPrice}>
+          <BasketGrid basketTotalPrice={totalPrice()} />
+        </Page>
       ),
     },
   ]);
