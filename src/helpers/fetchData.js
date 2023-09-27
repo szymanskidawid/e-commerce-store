a306613d08449a238fe3b3d0fd393f00c66ca878
Dawid <szynanski@hotmail.com>
2023-09-27 22:18:41 +0200

    changed connection to node server

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,7 +1,7 @@
 export const fetchData = async (setData) => {
-  const response = await fetch('http://localhost:4000/products');
+  const response = await fetch('https://e-commerce-store-backend.onrender.com/products');
 
   const dataJSON = await response.json();
 
-  setData(dataJSON);
+  setData(dataJSON.products);
 };
