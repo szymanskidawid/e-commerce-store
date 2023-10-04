001110718394cb6f9e50477d34de7d0219ac6a90
Dawid <szynanski@hotmail.com>
2023-10-04 19:20:50 +0200

    testing

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,5 +1,6 @@
 export const fetchData = async (setData) => {
-  const response = await fetch('https://e-commerce-store-backend.onrender.com/products');
+  //const response = await fetch('https://e-commerce-store-backend.onrender.com/products');
+  const response = await fetch('http://localhost:4000/products');
 
   const dataJSON = await response.json();
 
