a9a6f4d347ed15965efda4e7edc7ef3f0e2daea2
Dawid Szymanski <dawidszymanski.dev@outlook.com>
2023-10-15 21:41:55 +0200

    connected to Node server

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,8 +1,8 @@
 export const fetchData = async (setData) => {
   //const response = await fetch('https://e-commerce-store-backend.onrender.com/products');
-  const response = await fetch('http://localhost:4000/products');
+  const response = await fetch('http://localhost:4000');
 
   const dataJSON = await response.json();
 
-  setData(dataJSON.products);
+  setData(dataJSON);
 };
